a6cdf0f3e81e11df6a53d98cc74e11e0850efb52
Ivan <statkevich.ivan999@gmail.com>
2025-05-06 21:48:46 +0300

    Refactor Proxmox API configuration to use password instead of token; remove requirements.txt

--- config.py
+++ config.py
@@ -6,14 +6,16 @@ load_dotenv()
 
 host = os.getenv("PROXMOX_HOST")
 user = os.getenv("PROXMOX_USER")
-token_name = os.getenv("PROXMOX_TOKEN_NAME")
-token_value = os.getenv("PROXMOX_TOKEN_VALUE")
+password = os.getenv("PROXMOX_PASSWORD")
 verify_ssl = os.getenv("PROXMOX_VERIFY_SSL", "False").lower() == "true"
 
 proxmox = ProxmoxAPI(
     host,
-    user,
-    token_name,
-    token_value,
-    verify_ssl,
+    user=user,
+    password=password,
+    verify_ssl=verify_ssl,
 )
+
+for node in proxmox.nodes.get():
+    for vm in proxmox.nodes(node["node"]).qemu.get():
+        print(f"{vm['vmid']}. {vm['name']} => {vm['status']}")
