5d22e0db3d4c9b4fafa035c54611db69a42703be
Ivan <statkevich.ivan999@gmail.com>
2025-05-10 17:23:59 +0300

    Update LXC OS options in config.py to include Alpine, Arch, Fedora, and OpenSUSE images.

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ os_iso_dict = {
 }
 
 lxc_os_dict = {
-    "Ubuntu Server": "ubuntu-24.04.2-live-server-amd64.iso",
-    "CentOS Stream": "CentOS-Stream-10-latest-x86_64-dvd1.iso",
-    "Fedora Server": "Fedora-Server-netinst-x86_64-42-1.1.iso",
-    "Debian": "debian-12.10.0-amd64-netinst.iso",
+    "Alpine": "alpine-3.21-default_20241217_amd64.tar.xz",
+    "Arch": "archlinux-base_20240911-1_amd64.tar.zst",
+    "Fedora": "fedora-42-default_20250428_amd64.tar.xz",
+    "OpenSUSE": "opensuse-15.6-default_20240910_amd64.tar.xz",
 }
